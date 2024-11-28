6b610f6a36fa670813e22da63532a52805e73cb9
jiangshaoqi <jiangshaoqi9@163.com>
2024-11-27 20:40:43 -0500

    todo: read pb file and input into backchecker analysis

--- src/bin/main_h1_h2_h3_proxy.rs
+++ src/bin/main_h1_h2_h3_proxy.rs
@@ -104,7 +104,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let tcp_tls_acceptor = get_h2_config()?;
 
     // let tcp_listener = TcpListener::bind("127.0.0.1:443").await?;
-    let tcp_listener = TcpListener::bind("172.30.143.95:443").await?;
+    let tcp_listener = TcpListener::bind("172.30.143.69:443").await?;
     println!("Tcp binding finished");
 
 
@@ -117,7 +117,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         let endpoint = quinn::Endpoint::server(
             server_config,
             // SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 443),
-            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(172, 30, 143, 95)), 443),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(172, 30, 143, 69)), 443),
         )?;
         println!("Quic binding finished");
 
--- src/bin/raw_db_to_pb.rs
+++ src/bin/raw_db_to_pb.rs
@@ -1,14 +1,14 @@
-use std::{collections::HashSet, io::Read};
-
-use flate2::{bufread::DeflateDecoder, read::GzDecoder};
 use futures::StreamExt;
-use h3server::{RequestInCSV, RequestInMONGO};
+use h3server::RequestInMONGO;
 use mongodb::{ bson::doc, options::{ ClientOptions, ServerApi, ServerApiVersion }, Client, Collection };
 
-use csv::Writer;
-use serde_json;
+use std::{fs::OpenOptions, io::{BufWriter, Write}};
+// use serde_json;
 
-pub mod data;
+use prost::Message;
+
+
+include!(concat!(env!("OUT_DIR"), "/data.rs"));
 
 
 // take raw data into protobuf: .pb file
@@ -39,8 +39,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             "app": &package_name
         }).await?;
 
-    
 
+    // follow the step from backchecker: preprocessing.py: line 176
+    let data_dir = std::path::Path::new("pbdata");
+    // Create the data directory if it doesn't exist
+    if !data_dir.exists() {
+        std::fs::create_dir(data_dir)?;
+    }
+    let pb_name = format!("{}.pb", &package_name);
+    let file_path = data_dir.join(pb_name);
+    let pb_file = OpenOptions::new()
+        .write(true) // Open in write mode
+        .create(true) // Create the file if it does not exist
+        .append(true) // Append to the file, instead of overwriting
+        .open(file_path)?;
+    let mut writer = BufWriter::new(pb_file);
+
+
+    while let Some(Ok(req)) = cursor.next().await {
+
+        let req_method = req.method;
+        let req_uri = req.uri;
+        let req_headers = req.header;
+        let req_body = req.body;
+
+        let record_tls = req.tls;
+        let record_timestamp = req.time;
+
+        let mut req_headers_pb = Vec::new();
+        for (k, v) in req_headers {
+            let h_pb = Header { key: k, value: v };
+            req_headers_pb.push(h_pb);
+        }
+
+        let req_trailers_pb: Vec<Header> = Vec::new();
+
+        let request_pb = Request {
+            method: req_method,
+            path: req_uri,
+            headers: req_headers_pb,
+            trailers: req_trailers_pb,
+            body: req_body,
+        };
+
+        let record_websocket: Vec<WebSocketMessage> = Vec::new();
+
+        let conn_info_pb = ConnectionInfo {
+            source_address: "None".to_string(),
+            destination_address: "None".to_string(),
+            tls: record_tls,
+            timestamp: record_timestamp,
+        };
+
+        let record_pb = Record {
+            request: Some(request_pb),
+            response: None,
+            websocket: record_websocket,
+            connection_info: Some(conn_info_pb),
+        };
+
+        // write into file
+        let mut buf = Vec::new();
+        
+        record_pb.encode_length_delimited(&mut buf).unwrap();
+        writer.write_all(&buf)?;
+
+    }
+
+    writer.flush()?;
 
     Ok(())
 
